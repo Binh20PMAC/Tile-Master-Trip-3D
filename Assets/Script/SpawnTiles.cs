ad1a8f70e8ff6a3e3975756b811155cb9808c2a4
Trần Kais <0306201216@caothang.edu.vn>
2023-10-14 00:54:41 +0700

    Spawn level 1

---
+++ Assets/Script/SpawnTiles.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpawnTiles : MonoBehaviour
+{
+    [SerializeField] private GameObject tilePrefab;
+    [SerializeField] private int level;
+    [SerializeField] private List<GameObject> tileList;
+    void Start()
+    {
+        SpawnTilesPrefab();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void SpawnTilesPrefab()
+    {
+        float x = -3f;
+        float z = 6f;
+        for (int i = 0; i < level * 9; i++)
+        {
+            GameObject newTile = Instantiate(tilePrefab, transform);
+            newTile.name = "Tile " + i;
+            tileList.Add(newTile);
+            newTile.transform.localPosition = new Vector3(x, 2f, z);
+
+            if (x > 2f)
+            {
+                z -= 3f;
+                x = -3f;
+            }
+            else
+            {
+                x += 3f;
+            }
+        }
+    }
+
+}
